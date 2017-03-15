8d90b6b8c753faab087199b8d9152f7a1d376290
Jared Vogt <jared.vogt@gmail.com>
2017-03-14 20:51:49 -0700

    init commit

---
+++ install/bash.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+brew install bash
+
+grep "/usr/local/bin/bash" /private/etc/shells &>/dev/null || sudo bash -c "echo /usr/local/bin/bash >> /private/etc/shells"
+chsh -s /usr/local/bin/bash
