1ef4aefb402a84cb09560a617ea1945c6d26b41e
Jared Vogt <jared.vogt@gmail.com>
2019-05-31 14:13:57 -0700

    Update dock.sh

--- macos/dock.sh
+++ macos/dock.sh
@@ -5,8 +5,7 @@ dockutil --no-restart --add "/Applications/Google Chrome.app"
 dockutil --no-restart --add "/Applications/Firefox.app"
 dockutil --no-restart --add "/Applications/Safari.app"
 dockutil --no-restart --add "/Applications/iTerm.app"
-dockutil --no-restart --add "/Applications/Utilities/Activity Monitor.app"
-dockutil --no-restart --add "/Applications/MailPlane 3.app"
+dockutil --no-restart --add "/Applications/MailPlane 4.app"
 dockutil --no-restart --add "/Applications/Path Finder.app"
 
 killall Dock
